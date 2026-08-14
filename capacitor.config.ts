aa41f4135ab084dd69fdbd4bb6f68e5dc2ce8d97
Taher <taher.yakolawala@gmail.com>
2026-08-14 11:27:19 -0400

    chore: rename app to hennadocs

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.tampabayhenna.contracts",
-  appName: "Henna Contracts",
+  appName: "HennaDocs",
   webDir: "dist/renderer",
   android: {
     allowMixedContent: false,
